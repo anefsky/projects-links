08a7be64cb559d8e674306ad7baac04af5622f47
andrew nefsky <anefsky@gmail.com>
2020-03-01 12:56:12 -0500

    Changed project list

--- src/assets/ProjectItems.js
+++ src/assets/ProjectItems.js
@@ -3,7 +3,7 @@ const listings = [
         name: "Snake Game",
         source: "https://github.com/anefsky/snake-game",
         hosted: "https://andrewnefsky.com/projects/snake-game/build",
-        description: "React implementation of classic snake game."
+        description: "React implementation of classic video game."
     },
     {
         name: "Clock",
@@ -16,13 +16,7 @@ const listings = [
         name: "Purchasing app",
         source: "https://github.com/anefsky/penguin",
         hosted: "https://andrewnefsky.com/projects/penguin",
-        description: "Internal business application used for coordinating pricing between bidders and buyers.  Uses angular, sass, bootstrap."
-    },
-    {
-        name: "Ecommerce app",
-        source: "https://github.com/anefsky/catalogue_page",
-        hosted: "https://andrewnefsky.com/projects/catalogue_page",
-        description: "Prototype for single page ecommerce app.  Uses angular, sass."
+        description: "Business application used for price bidding.  Uses angular, sass, bootstrap."
     }
 ];
 
